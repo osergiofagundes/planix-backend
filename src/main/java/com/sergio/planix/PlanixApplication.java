93f21f5663271602d95c08e368663c65c8259b05
Sergio Fagundes <contato.sergiofagundes@gmail.com>
2026-07-26 22:47:13 -0300

    first commit

---
+++ src/main/java/com/sergio/planix/PlanixApplication.java
@@ -0,0 +1,13 @@
+package com.sergio.planix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlanixApplication.class, args);
+	}
+
+}
